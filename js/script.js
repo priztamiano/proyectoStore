f87f4bf1aa2e2bfcc2769e24a2a737e986fe968c
Priz Tamiano <priz.candy@gmail.com>
2019-02-13 00:23:49 -0300

    Added files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// MODAL MODAL MODAL
+let modal = document.getElementById("miModal");
+
+    function abrir()  {
+        modal.style.display = "block";
+    }
+
+    function cerrar() {
+        modal.style.display = "none";
+    }
+// MODAL MODAL MODAL
+
+//SEARCH SEARCH SEARCH
+
+//SEARCH SEARCH SEARCH
